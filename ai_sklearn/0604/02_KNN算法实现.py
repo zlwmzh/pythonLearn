9cb3c7828ee2e45ef536ab9c5f7047aa7c2255be
Micky <1354319679@qq.com>
2019-06-05 20:28:53 +0800

    2019/06/05晚KNN算法实现，kdtree查找，加权多数表决

--- ai_sklearn/0604/02_KNN算法实现.py
+++ ai_sklearn/0604/02_KNN算法实现.py
@@ -11,14 +11,18 @@
       认为在空间上相近的样本具有相同的特征属性。“近朱者赤，近墨者黑”
 
   -2. KNN算法步骤：
-      a. 从训练数据中获取和待预测样本最相似的K个样本
-      b. 将K个样本的目标属性合并产生最终的预测值(多数投票)
+      a. 找出离待测样本最近的K个训练样本点
+      b. 根据获取的K个样本数据预测待测样本的目标属性
 
   -3. KNN算法缺陷：计算待测样本与训练样本的距离计算量大。使用KDTree进行优化
 """
 import numpy as np
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+
 
 class MickyKNNClassic(object):
     def __init__(self, k = 3):
@@ -38,10 +42,14 @@ class MickyKNNClassic(object):
     predict_X : 
     """
     def predict(self,predict_X):
+        predict_list = []
         # 遍历预测样本
         for x in predict_X:
             # 找到x的k个最近样本的目标属性
-            pass
+            neighbors = self.fetch_k_neighbors(x)
+            # 找出k个最相似样本中出现最多的目标属性
+            predict_list.append(self.calc_max_count_label(neighbors))
+        return predict_list
 
     """
     查找待测样本点的K个最近点
@@ -52,7 +60,36 @@ class MickyKNNClassic(object):
             # 计算欧式距离
             dist = np.sqrt(np.sum((x - predict_x) ** 2))
             dist_label_list.append((dist,y))
-        print (sorted(dist_label_list))
+        dist_label_list = sorted(dist_label_list)
+        dist_label_index = list(map(lambda t : t[1],dist_label_list[:self.k]))
+        return dist_label_index
+
+    """
+    找出出现最多的那个目标属性
+    """
+    def calc_max_count_label(self, neighbors):
+        # 1. 统计目标属性出现的个数
+        label_dict = {}
+        for y in neighbors:
+            if y not in label_dict:
+                label_dict[y] = 1
+            else:
+                label_dict[y] += 1
+        # 2. 找出出现个数最多的目标属性
+        max_count = 0
+        max_count_label = -1
+        for label,count in label_dict.items():
+            if count > max_count:
+                max_count = count
+                max_count_label = label
+        return max_count_label
+
+    """
+    模型评估
+    """
+    def score(self, X, y, sample_weight=None):
+        return accuracy_score(y,self.predict(X),sample_weight=sample_weight)
+
 
 if __name__ == '__main__':
     # 1. 数据加载
@@ -66,9 +103,18 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=214)
     # 4. 特征工程
     # 5. 模型对象构建
-    knnC = MickyKNNClassic()
+    knnC = MickyKNNClassic(k=5)
     # 6. 模型训练
     knnC.fit(x_train,y_train)
-    # print(x_train)
-    knnC.fetch_k_neighbors(x_train)
+    # 7. 模型效果评估
+    print("训练集模型效果:{}".format(knnC.score(x_train,y_train)))
+    print("测试集模型效果:{}".format(knnC.score(x_test, y_test)))
+    # print(knnC.predict(x_test))
+    # 模型持久化
+    filename = './models/MickyKNNClassic.pkl'
+    dir_path = os.path.dirname(filename)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    joblib.dump(value=knnC,filename=filename)
+
 
--- ai_sklearn/0605/02_KNN算法代码实现.py
+++ ai_sklearn/0605/02_KNN算法代码实现.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 from sklearn.neighbors import KDTree
+from sklearn.metrics import accuracy_score
 
 class KNN(object):
 
@@ -47,13 +48,14 @@ class KNN(object):
             # 0. 如果预测样本点就是训练集中的点，直接返回目标值即可，不必执行接下来的操作
             index = np.argwhere(np.logical_and.reduce(x == train_X, axis=1) == True)
             if len(index) > 0:
-                print("测试样本中包含，直接返回目标值")
-                predict_label.append(train_Y[index[0]])
+                # print("测试样本中包含，直接返回目标值")
+                predict_label.append(train_Y[index[0]][0])
                 continue
             # if x in train_X:
             #     continue
             # 1. 找出K个最相似的样本(距离上最近)
             k_neighbors_dist, k_neighbors_index = self.fetch_k_neighbors(x, return_distance = True)
+            # print(k_neighbors_dist,k_neighbors_index)
             # 2. 计算出K个最相似样本出现最多的目标属性即为预测值
             predict_label.append(self.calc_max_count_label(k_neighbors_dist, k_neighbors_index, self.max_count_type))
         return predict_label
@@ -70,7 +72,7 @@ class KNN(object):
            return self.query_by_kdtree(predict_x, return_distance= return_distance)
         else:
             # 暴力查找
-            pass
+           return self.query_by_all(predict_x, return_distance= return_distance)
 
     """
     KDTree查找最相似的K个样本
@@ -84,6 +86,21 @@ class KNN(object):
            label_index_L = self.tree.query([predict_x], k=self.k_neighbors, return_distance=return_distance)[0]
            return label_index_L
 
+    """
+    暴力查找最相似的K个样本
+    """
+    def query_by_all(self, predict_x, return_distance= True):
+        index = 0
+        label_dict = []
+        for x in train_X:
+            dict = np.sqrt(np.sum((x-predict_x) ** 2))
+            label_dict.append((dict,index))
+            index += 1
+        label_dict = sorted(label_dict)
+        if return_distance:
+            return list(map(lambda d : d[0], label_dict[:self.k_neighbors])), list(map(lambda d : d[1], label_dict[:self.k_neighbors]))
+        else:
+            return list(map(lambda d : d[1], label_dict[:self.k_neighbors]))
 
     """
     找出待测点的目标属性
@@ -97,7 +114,7 @@ class KNN(object):
             return self.calc_max_count_label_by_max_count(train_Y[k_neighbors_index])
         else:
             # 加权多数表决法
-            self.calc_max_count_label_by_max_count_weight(k_neighbors_dist, k_neighbors_index)
+            return self.calc_max_count_label_by_max_count_weight(k_neighbors_dist, k_neighbors_index)
 
     """"
     通过多数表决法找出
@@ -124,22 +141,33 @@ class KNN(object):
     k_neighbors_index：最近的K个样本点的下标
     """
     def calc_max_count_label_by_max_count_weight(self, k_neighbors_dist, k_neighbors_index):
-        # 计算权重
-        k_neighbors_weight = [1/dist  for dist in k_neighbors_dist if dist != 0]
-        # k个最相似样本的目标属性
         k_neighbors_y = train_Y[k_neighbors_index]
-        # 统计目标属性个数
-        label_count_dict = {}
+        index = 0
+        label_weight_dict = {}
         for y in k_neighbors_y:
-            if y in label_count_dict:
-                label_count_dict[y] += 1
+            # 计算权重,取反比
+            weight = 1/k_neighbors_dist[index]
+            if y in label_weight_dict:
+                label_weight_dict[y] += weight
             else:
-                label_count_dict[y] = 1
-        # 计算权重
-        k_neighbors_label_weight = {}
-        for y,index in zip(k_neighbors_y,k_neighbors_index):
-            print(label_count_dict[y] * index)
-        # print(k_neighbors_label_weight)
+                label_weight_dict[y] = weight
+            index += 1
+        # 找出权重最大的目标属性
+        max_weight = 0
+        max_weight_label = -1
+        for label,weight in label_weight_dict.items():
+            if weight > max_weight:
+                max_weight = weight
+                max_weight_label = label
+        return max_weight_label
+
+    """
+       模型评估
+       """
+
+    def score(self, X, y, sample_weight=None):
+        return accuracy_score(y, self.predict(X), sample_weight=sample_weight)
+
 
 if __name__ == '__main__':
     # 1. 数据加载
@@ -160,9 +188,10 @@ if __name__ == '__main__':
     # 3. 训练数据和测试数据划分(创建数据时候已经划分)
     # 4. 特征工程
     # 5. 创建模型对象
-    algo = KNN(k_neighbors = 4,with_kdtree = True,max_count_type='max_count_add_weight')
+    algo = KNN(k_neighbors = 4,with_kdtree = True, max_count_type='max_count_add_weight')
     # 6. 训练模型
     algo.fit(train_X, train_Y)
+
 
     X = np.asarray([
         [1, 3],
@@ -170,5 +199,9 @@ if __name__ == '__main__':
         [7, 9],
         [6, 9]
     ])
-    algo.predict(X)
+    Y = np.array([0, 0, 1, 1])
+    # 模型效果评估
+    print("训练集效果：{}".format(algo.score(train_X,train_Y)))
+    print("测试集效果：{}".format(algo.score(X, Y)))
+    print(algo.predict(X))
 
